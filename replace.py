0672b32c44c48313e044cca94658b0da6a864e46
Mike Schwartz <SchwartzMPNYC@gmail.com>
2018-09-04 03:17:45 +0000

    Initial commit of Regex Replacer

---
+++ replace.py
@@ -0,0 +1,34 @@
+import sublime
+import sublime_plugin
+
+regex_strings = {'styles':' style=".*?"', 'tables':'</?(table|tbody|thead|tr|td)>'}
+
+class EraseAllTypesCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for regex_str in regex_strings:
+			Erase.erase(self.view, edit, regex_strings[regex_str])
+
+class EraseAllTableTypesCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		Erase.erase(self.view, edit, regex_strings['tables'])
+
+class EraseAllInlineStylesCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		Erase.erase(self.view, edit, regex_strings['styles'])
+
+
+class EraseInputCommand(sublime_plugin.TextCommand):
+	def run(self, edit, text):
+		self.input(text)
+		Erase.erase(self.view, edit, text)
+
+	def input(self, text):
+		return sublime_plugin.TextInputHandler()
+
+class Erase():
+	def erase(view, edit, to_match):
+		offset = 0
+		for match in view.find_all(to_match):
+			view.erase(edit, sublime.Region(match.a-offset, match.b-offset))
+			offset += match.size()
+		
